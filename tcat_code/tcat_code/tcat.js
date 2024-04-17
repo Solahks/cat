5e20868e302faf022522eae0a6e2dae034b7d302
Solah <148382884+Solahks@users.noreply.github.com>
2024-04-17 12:49:39 -0500

    remove js line 8
    uneeded mysrc line removed

---
+++ tcat_code/tcat_code/tcat.js
@@ -0,0 +1,53 @@
+myCat = document.getElementById("tcat");
+const eyeClose = setInterval(myCatClose, 3500);
+const eyeOpen = setInterval(myCatOpen, 3550);
+const mouthOpen = setInterval(catMouthOpen, 11500);
+const mouthClose = setInterval(catMouthClose, 14500);
+
+function catPet() {
+  myCat.setAttribute("src", "images/tcat4.png");
+  const stopPets = setTimeout(endCatPets, 500);
+  console.log("pet");
+}
+
+function myCatClose() {
+  const mySrc = myCat.getAttribute("src");
+  if (mySrc === "images/tcat.png") {
+    myCat.setAttribute("src", "images/tcat2.png");
+  }
+}
+
+function myCatOpen() {
+  const mySrc = myCat.getAttribute("src");
+  if (mySrc === "images/tcat2.png") {
+    myCat.setAttribute("src", "images/tcat.png");
+    setInterval(myCatOpen, 3500);
+    console.log("open");
+  }
+  clearInterval(eyeOpen);
+}
+
+function catMouthOpen() {
+  const mySrc = myCat.getAttribute("src");
+  if (mySrc === "images/tcat.png") {
+    myCat.setAttribute("src", "images/tcat3.png");
+  }
+}
+
+function catMouthClose() {
+  const mySrc = myCat.getAttribute("src");
+  if (mySrc === "images/tcat3.png") {
+    myCat.setAttribute("src", "images/tcat.png");
+    setInterval(catMouthClose, 11500);
+    console.log("mouth close");
+  }
+  clearInterval(mouthClose);
+}
+
+function endCatPets() {
+    const mySrc = myCat.getAttribute("src");
+    if (mySrc === "images/tcat4.png") {
+      myCat.setAttribute("src", "images/tcat.png");
+    }
+    console.log("endpets")
+}
